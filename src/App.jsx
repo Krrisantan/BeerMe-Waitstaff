c58b6b93c6d73b724c25aab79d2aa84e53fcfab6
Kristopher <Krrisantan@proton.me>
2024-04-15 04:15:28 -0700

    final submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import './App.scss';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import "./App.scss";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-
   const [newOrders, setNewOrders] = useState([]);
   useEffect(() => {
     const getNewOrders = async () => {
@@ -14,18 +13,17 @@ function App() {
       } catch (error) {
         console.error("Failed to fetch new orders list:", error);
       }
-    }
+    };
     getNewOrders();
-  }, [newOrders])
+  }, [newOrders]);
 
   const removeOrderHandler = async (currentOrder) => {
-    console.log(currentOrder)
-    console.log('order should be removed')
-    const deleteOrderUrl = `http://localhost:8080/ordersRoutes/orders/${currentOrder.id}`
-    console.log(deleteOrderUrl)
+    console.log(currentOrder);
+    console.log("order should be removed");
+    const deleteOrderUrl = `http://localhost:8080/ordersRoutes/orders/${currentOrder.id}`;
+    console.log(deleteOrderUrl);
     const response = await axios.delete(deleteOrderUrl);
-    //make an axios  to localhost:8080/ordersRoutes/orders/{currentOrder.id}
-  }
+  };
 
   return (
     <div className="App">
@@ -33,7 +31,6 @@ function App() {
         <table>
           <thead>
             <tr>
-             
               <th>Table #</th>
               <th>Name</th>
               <th>Quantity</th>
@@ -44,13 +41,19 @@ function App() {
             {newOrders.map((currentOrder, key) => {
               return (
                 <tr key={key}>
-                  
                   <td>{currentOrder.table_number}</td>
                   <td>{currentOrder.name}</td>
                   <td>{currentOrder.quantity}</td>
-                  <td><button className='remove' onClick={() => removeOrderHandler(currentOrder)}>Remove Order</button></td>
+                  <td>
+                    <button
+                      className="remove"
+                      onClick={() => removeOrderHandler(currentOrder)}
+                    >
+                      Remove Order
+                    </button>
+                  </td>
                 </tr>
-              )
+              );
             })}
           </tbody>
         </table>
